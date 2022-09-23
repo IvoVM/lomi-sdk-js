daaf12a958b5cd26dd1800e96c3eac94e7a7444b
orlando-abm <obernalesm@gmail.com>
2022-09-23 16:16:19 -0300

    Add show email and rol on settings

--- packages/lomi-backoffice/settings/settings/settings.component.ts
+++ packages/lomi-backoffice/settings/settings/settings.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { BackofficeState } from 'packages/lomi-backoffice/ngrx';
+import { currentUserSelector } from 'packages/lomi-backoffice/ngrx/reducers/user.reducer';
 import { LastMileProvidersService } from 'packages/lomi-backoffice/src/app/providers/last-mile-providers.service';
 
 @Component({
@@ -11,14 +14,17 @@ export class SettingsComponent implements OnInit {
   userSettings: any = {};
   appSettings: any = {};
   lastMileProviders:any = [];
-
   constructor(
     private journeyProviders: LastMileProvidersService,
+    private store:Store<BackofficeState>,
   ) {}
 
   ngOnInit(): void {
     this.journeyProviders.lastMileProviders$.subscribe((providers:any) => {
       this.lastMileProviders = Object.values(providers);
+    })
+    this.store.select(currentUserSelector).subscribe((user:any)=>{
+      if (user) this.userSettings = user
     })
   }
 }
