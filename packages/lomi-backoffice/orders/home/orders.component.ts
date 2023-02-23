9c17f9be98045e2d6518a14fd7cf970876e3274e
orlando-abm <obernalesm@gmail.com>
2023-02-23 15:54:32 -0300

    Go to tab status order

--- packages/lomi-backoffice/orders/home/orders.component.ts
+++ packages/lomi-backoffice/orders/home/orders.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { OrdersService } from '../../providers/lomi/orders.service';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
@@ -29,7 +29,19 @@ export class OrdersComponent implements OnInit {
       stockLocationId: [null],
       deliveryState: ['Estado envio']
     })
+
+    this.router.events.subscribe(async (event: any) => {
+      if (event.anchor) {
+        const st = localStorage.getItem('stockLocationId')
+        const getOrder = await this.ordersProvider.getOrder(event.anchor, st)
+        const order: any = getOrder.data()
+        if (!order) return
+        const { status } = order
+        this.ordersProvider.currentStep = status
+      }
+    })
   }
+
 
   stateName(state:any){
     switch(state){
